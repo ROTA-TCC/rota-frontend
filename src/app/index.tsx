00ec910c86caf9366cd30778ea848cf0b72f0bce
Rafael Martins Paiva <rafael.martins.paiva0@gmail.com>
2026-09-01 09:47:42 -0300

    .

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,19 +8,19 @@ import Animated, {
   Easing,
 } from 'react-native-reanimated';
 
-const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 export default function SplashScreen() {
   const iconTranslateY = useSharedValue(SCREEN_HEIGHT);
   const iconScale = useSharedValue(3);
-  const iconRotate = useSharedValue(0);
+  const iconRotate = useSharedValue(20);
   const logoOpacity = useSharedValue(0);
 
   useEffect(() => {
     // Icon animation: rise, rotate, shrink
     iconTranslateY.value = withTiming(0, { duration: 1500, easing: Easing.bezier(0.25, 0.1, 0.25, 1) });
     iconScale.value = withTiming(1, { duration: 1500 });
-    iconRotate.value = withTiming(360, { duration: 1500 });
+    iconRotate.value = withTiming(380, { duration: 1500 });
 
     // Logo appears after icon animation
     logoOpacity.value = withDelay(1500, withTiming(1, { duration: 500 }));
@@ -69,7 +69,8 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     position: 'absolute',
-    // Positioned slightly to the left of the center
-    left: 40,
-  },
+    // Positioned 67 pixels to the left of the center
+    left: SCREEN_WIDTH / 2 - 67,
+    },
+
 });
