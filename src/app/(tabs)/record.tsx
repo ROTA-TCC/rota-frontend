ffe46a45ad3152260bfc0945e54d33f52e33824e
Rafael Martins Paiva <rafael.martins.paiva0@gmail.com>
2026-09-25 09:04:04 -0300

    .

---
+++ src/app/(tabs)/record.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function RecordScreen() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 42.882004,
+          longitude: 74.582748,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        showsUserLocation={true}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
