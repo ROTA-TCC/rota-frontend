9a34eb9d8492d99eeb56fd1c5326e76225732d5d
Rafael-Martins-Paiva <rafael.martins.paiva0@gmail.com>
2026-09-26 15:02:12 -0300

    update FloatingButtons.tsx

--- src/components/Record/FloatingButtons.tsx
+++ src/components/Record/FloatingButtons.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { MapFab } from '../../map/MapFab';
+import { MapFab } from '../map/MapFab';
 
 type FloatingButtonsProps = {
   onBack?: () => void;
